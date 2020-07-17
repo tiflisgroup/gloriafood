ace7237169480b349c4251816f2215bb9f17543e
Gabriele Canepa <git@gabrielecanepa.com>
2020-07-17 22:38:53 +0200

    Add Slackbot

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const dotenv = require('dotenv')
 const request = require('request')
 const tldEnum = require('tld-enum')
 const tldExtract = require('tld-extract')
+const SlackBot = require('slackbots');
 
 dotenv.config()
 
@@ -15,34 +16,66 @@ const gloriafoodOptions = {
   json: true
 }
 
-request(gloriafoodOptions, (error, response, body) => {
-  if (error) throw new Error(error)
-
-  const clients = body.rows.filter(client => tldEnum.list.includes(tldExtract(`https://www.${client.email.split('@')[1]}`).tld))
-  const contacts = clients.map(client => ({
-    email: client.email,
-    properties: [
-      { property: 'firstname', value: client.first_name },
-      { property: 'lastname', value: client.last_name },
-      { property: 'email', value: client.email },
-      { property: 'phone', value: client.phone },
-      { property: 'orders', value: client.orders_count },
-      { property: 'total', value: client.orders_value },
-      { property: 'pizzeria', value: true }
-    ]
-  }))
-
-  // Post contacts to HubSpot
-  const hubspotOptions = {
-    method: 'POST',
-    url: `https://api.hubapi.com/contacts/v1/contact/batch`,
-    qs: { hapikey: process.env.HUBSPOT_API_KEY },
-    body: contacts,
-    json: true
-  }
-
-  request(hubspotOptions, (error, response, body) => {
-    if (error) throw error
-    if (body && body.failureMessages) throw body.failureMessages
-  })
+const bot = new SlackBot({
+  token: process.env.SLACKBOT_API_KEY,
+  name: 'GloriaFood',
 })
+
+const parseClient = client => ({
+  email: client.email,
+  properties: [
+    { property: 'firstname', value: client.first_name },
+    { property: 'lastname', value: client.last_name },
+    { property: 'email', value: client.email },
+    { property: 'phone', value: client.phone },
+    { property: 'orders', value: client.orders_count },
+    { property: 'total', value: client.orders_value },
+    { property: 'pizzeria', value: true }
+  ]
+})
+
+const postSuccessMessageToTechChannel = () => bot.postMessageToChannel('tech', `
+  ✅ Contacts successfully migrated to HubSpot :hubspot: See them live *<https://app.hubspot.com/contacts/8109649|here>*.
+`, {
+  icon_emoji: ':gloriafood'
+})
+
+const postErrorMessageToTechChannel = error => bot.postMessageToChannel('tech', `
+  ❌ There was an error with running the contacts migration. See the full logs *<https://dashboard.heroku.com/apps/gloriafood/logs|here>*
+`, {
+  icon_emoji: ':gloriafood:'
+})
+
+const runPlugin = () => {
+  request(gloriafoodOptions, (error, _, body) => {
+    if (error) {
+      postErrorMessageToTechChannel(error)
+      throw error
+    }
+
+    const clients = body.rows.filter(client => tldEnum.list.includes(tldExtract(`https://www.${client.email.split('@')[1]}`).tld))
+    const contacts = clients.map(client => parseClient(client))
+
+    const hubspotOptions = {
+      method: 'POST',
+      url: `https://api.hubapi.com/contacts/v1/contact/batch`,
+      qs: { hapikey: process.env.HUBSPOT_API_KEY },
+      body: contacts,
+      json: true
+    }
+
+    request(hubspotOptions, (error, response, body) => {
+      if (error) {
+        postErrorMessageToTechChannel(error)
+        throw error
+      }
+      if (body && body.failureMessages) {
+        postErrorMessageToTechChannel(body.failureMessages)
+        return
+      }
+      postSuccessMessageToTechChannel()
+    })
+  })
+}
+
+bot.on('start', runPlugin)
