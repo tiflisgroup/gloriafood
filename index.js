5e0b9fc12d80a77e143046aebde408d91a83b68e
Gabriele Canepa <git@gabrielecanepa.com>
2023-02-25 16:51:13 +0100

    Cleanups

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv')
 const request = require('request')
 const tldEnum = require('tld-enum')
 const tldExtract = require('tld-extract')
-const SlackBot = require('slackbots');
+const SlackBot = require('slackbots')
 
 const GLORIAFOOD_API_CLIENTS_URL = 'https://www.gloriafood.com/api/stats/clients_list'
 const HUBSPOT_API_CONTACTS_URL = 'https://api.hubapi.com/contacts/v1/contact/batch'
@@ -14,9 +14,9 @@ const gloriafoodOptions = {
   url: GLORIAFOOD_API_CLIENTS_URL,
   headers: { Authorization: process.env.GLORIAFOOD_API_KEY },
   body: {
-    limit: 1_000_000
+    limit: 1000000,
   },
-  json: true
+  json: true,
 }
 
 const bot = new SlackBot({
@@ -32,21 +32,31 @@ const parseClient = client => ({
     { property: 'email', value: client.email },
     { property: 'phone', value: client.phone },
     { property: 'orders', value: client.orders_count },
-    { property: 'total', value: client.orders_value }  
-  ]
+    { property: 'total', value: client.orders_value },
+  ],
 })
 
-const postSuccessMessageToTechChannel = () => bot.postMessageToChannel('tech', `
+const postSuccessMessageToTechChannel = () =>
+  bot.postMessageToChannel(
+    'tech',
+    `
   ✅ Contacts successfully migrated to HubSpot :hubspot: See them live *<https://app.hubspot.com/contacts/${process.env.HUBSPOT_ACCOUNT_ID}|here>*.
-`, {
-  icon_emoji: ':gloriafood'
-})
+`,
+    {
+      icon_emoji: ':gloriafood',
+    },
+  )
 
-const postErrorMessageToTechChannel = () => bot.postMessageToChannel('tech', `
+const postErrorMessageToTechChannel = () =>
+  bot.postMessageToChannel(
+    'tech',
+    `
   ❌ There was an error with running the contacts migration. See the full logs *<https://dashboard.heroku.com/apps/${process.env.HEROKU_APP_NAME}/logs|here>*
-`, {
-  icon_emoji: ':gloriafood:'
-})
+`,
+    {
+      icon_emoji: ':gloriafood:',
+    },
+  )
 
 const runPlugin = () => {
   request(gloriafoodOptions, (error, _, body) => {
@@ -55,7 +65,9 @@ const runPlugin = () => {
       throw error
     }
 
-    const clients = body.rows.filter(client => tldEnum.list.includes(tldExtract(`https://www.${client.email.split('@')[1]}`).tld))
+    const clients = body.rows.filter(client =>
+      tldEnum.list.includes(tldExtract(`https://www.${client.email.split('@')[1]}`).tld),
+    )
     const contacts = clients.map(client => parseClient(client))
 
     const hubspotOptions = {
@@ -63,7 +75,7 @@ const runPlugin = () => {
       url: HUBSPOT_API_CONTACTS_URL,
       qs: { hapikey: process.env.HUBSPOT_API_KEY },
       body: contacts,
-      json: true
+      json: true,
     }
 
     request(hubspotOptions, (error, response, body) => {
